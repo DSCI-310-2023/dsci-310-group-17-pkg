0e5b2c12fc45c59a5c67825a09c8a72f6c838494
Matthew Gillies <mcgillies@shaw.ca>
2023-04-07 11:44:29 -0700

    documented eda_script.py

--- src/group17pkg/eda_script.py
+++ src/group17pkg/eda_script.py
@@ -7,18 +7,33 @@ from eda import plot_correlations
 
 def main(input_path, output_folder):
     """
-    Creates EDA figure and table based on clean data
-    Stores them at the given output folder
+    Takes in a dataframe from the input_path, creates a directory for the output folder if not already
+    present, and then creates two figures with the dataframe, a table produced by df.describe(), and
+    a correlation matrix using plot_correlations. Then saves these figures to the desired folder with 
+    given filenames. Also uses argument parser to prompt the user and provide help descriptions. 
 
     Parameters
-    ----------
-    input_path: (string) -> path to clean data
+    -------
+    input_path: String
+        The path of the dataframe to input
 
-    output_path: (string) -> stores figures and CSVs to this file
+    output_folder: String
+        The path of the folder that is the desired location for the figures produced. 
     
     Returns
-    ----------
-    None
+    -------
+    None. Assigns the figures to the given folder with file names "/data_described.csv" and
+    "/correlation_of_numeric_features.png". 
+
+    Example
+    -------
+    Inputting a dataframe and recieving EDA figures as output
+    >>> import hyperthyroid_classifier as hyp
+        Inputting the specified paths into the function 
+    >>> hyp.main("hyperthyroid.csv", "results/figures")
+        The figures should now be found at "results/figures/data_described.csv" and 
+        "results/figures/correlation_of_numeric_features.png"
+
     """
     df = pd.read_csv(input_path)
     if not os.path.exists(output_folder):
