dbe32f6e6fb003e77b3f15ebf36a27ea9b70de1d
Matthew Gillies <mcgillies@shaw.ca>
2023-04-08 13:59:37 -0700

    documented analysis_visualizations.py

--- src/group17pkg/analysis_visualizations.py
+++ src/group17pkg/analysis_visualizations.py
@@ -3,16 +3,29 @@ import matplotlib.pyplot as plt
 
 def visualize_classification(data, preds):
     """
-    Produces a scatteplot with the classified data points for the  given dataset
+    Produces a scatteplot with the classified data points for the given dataset. The scatterplot has 
+    TSH concentration on the x-axis and TT4 concentration on the y-axis, with points coloured by
+    the prediction 
 
     Parameters
-    ----------
-    data: (pd.DataFrame) -> the dataset the predictions are taken from
-    preds: (pd.DataFrame) -> the classification predictions that have been produced
+    -------
+    data: pd.DataFrame:
+        The dataset the predictions are taken from. 
+    preds: pd.DataFrame:
+         The classification predictions that have been produced. Should contain labels of P or N for
+         each row in the dataframe
 
     Returns
-    ----------
+    -------
     fig: Scatterplot object visualizing the classifications of the data points
+
+    Example
+    -------
+    Produce a scatterplot analyzing the classification of hyperthyroid disease
+    >>> import hyperthyroid_classifier as hyp
+        Inputting the dataframe along with the prediction set to use visualize_classification
+    >>> hyp.visualiaze_classification(hyper_train, train_preds)
+        This will produce a scatterplot visualizing the predictions for the hyper_train dataset. 
     """
     fig = plt.figure()
     plt.scatter(data["TSH"], data["TT4"], c=preds, s=50, cmap='viridis')
