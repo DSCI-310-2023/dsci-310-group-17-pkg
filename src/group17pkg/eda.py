aff94b1cfc7f15de20d5c8e64e955c608184e368
Matthew Gillies <mcgillies@shaw.ca>
2023-04-08 14:03:17 -0700

    documented eda.py

--- src/group17pkg/eda.py
+++ src/group17pkg/eda.py
@@ -5,15 +5,25 @@ import seaborn as sns
 def plot_correlations(data):
     """
     Takes in the cleaned dataset, selects only numerical features, and then calculates 
-    the correlations between them and plots them as a heatmap
+    the correlations between them and plots them as a heatmap. 
 
     Parameters
-    ----------
-    data: (pd.DataFrame) -> the dataset in which correlations are to be produced from
+    -------
+    data: pd.DataFrame: 
+        The dataset in which correlations are to be produced from. Contains the columns 'age', 'TSH', 
+        'TT4', 'T4U', and 'FTI'
 
     Returns
-    ----------
-    fig: Heatmap visualizing the correlations of numeric features
+    -------
+    fig: Heatmap visualizing the correlations of numeric features. 
+
+    Example
+    -------
+    Visualize the correlations between the numeric features of the hyperthyroid dataset
+    >>> import hyperthyroid_classifier as hyp
+        Input the dataset as a parameter to use plot_correlations
+    >>> hyp.visualize(hyperthyroid)
+        This will return a heatmap representing the correlations of the numeric features of the dataset
     """
     print("Figure 1: Correlations of numeric features")
     numeric = data[['age', 'TSH', 'TT4', 'T4U', 'FTI']]
